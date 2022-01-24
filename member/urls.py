597d1ece14bac2f9ab03c6e9767b7cd83e025c68
Bae <qoru588@naver.com>
2022-01-24 14:57:39 +0900

    a

--- member/urls.py
+++ member/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
-from django.contrib.auth import views
+from . import views
+
 app_name = 'member'
 
 urlpatterns = [
