9eea7a047a422b73ca5083ce620bbbc1d9203530
acogange <acogange2@naver.com>
2022-01-25 16:35:59 +0900

    h

--- mapquiz/views.py
+++ mapquiz/views.py
@@ -5,6 +5,7 @@ from .models import Place
 import random, json
 from django.contrib.auth.decorators import login_required
 from django.http import JsonResponse
+import rank
 @login_required
 
 def quiz(request):
@@ -12,6 +13,7 @@ def quiz(request):
     num = random.randrange(0,len(place_list))
 
     answer = place_list[num]
+    rank.near_food(request,answer)
     latlong = {
         'lat' : answer.place_lat,
         'long' : answer.place_long
