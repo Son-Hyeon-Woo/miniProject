1620c3fc01b91a2aa9f9a7be77d569fe88cf10ba
acogange <acogange2@naver.com>
2022-01-25 14:09:56 +0900

    jj

--- rank/views.py
+++ rank/views.py
@@ -1,13 +1,19 @@
 from platform import java_ver
 from django.shortcuts import render
-
+from rank.models import Around_place
 def finish(request):
     
     return render(request, 'rank/finish.html')
 
 def near_food(request):
+    around_place=Around_place.objects.all()
+    result=[]
     
-    return render(request, 'rank/near_food.html')
+    for c in around_place:
+        if c.id_p==4:   #4==선택한 place의 id
+            result.append(c.name)
+
+    return render(request, 'rank/near_place.html', { 'data': result })
 
 def near_place(request):
     
