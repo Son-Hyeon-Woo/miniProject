95c00ea747c1868960d18e9512290cec98482039
minsu <excelsu1@gmail.com>
2022-01-25 11:24:52 +0900

    post컬럼추가

--- placeboard/urls.py
+++ placeboard/urls.py
@@ -14,8 +14,10 @@ urlpatterns = [
     path('blog/<int:pk>/', posting, name='posting'),
     path('blog/new_post/', new_post, name='new_post'),
     path('blog/<int:pk>/remove/', remove_post, name='remove_post'),
-]
+]+ static(
+settings.MEDIA_URL,
+document_root=settings.MEDIA_ROOT)
 
 # 이미지 URL 설정
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
