9eadaef7400bdda51a9ecd3fd5761b5db4cdbdf4
sonhw <ths1611@naver.com>
2022-01-26 13:11:14 +0900

    0126 퀴즈로그데이터베이스

--- rank/views.py
+++ rank/views.py
@@ -1,6 +1,11 @@
 from platform import java_ver
 from django.shortcuts import render
 from rank.models import Around_place
+import json
+from django.http import JsonResponse, HttpResponse
+from django.shortcuts import redirect
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
+from mapquiz.models import QuizLog
 
 
 def finish(request):
@@ -20,4 +25,10 @@ def near_place(request):
     return render(request, 'rank/near_place.html')
 
 def ranking(request):
+    if request.method == 'POST':
+        QuizLog.objects.create(
+            user_id = request.POST['username'],
+            point = request.POST['point'],
+            play_time = request.POST['play_time']
+        )
     return render(request, 'rank/ranking.html')
