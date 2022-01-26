1fad0c308a297028864b81a41c3989145d3941b4
Jimi <wonside99@naver.com>
2022-01-26 15:23:07 +0900

    비밀번호 변경 완료

--- mypage/models.py
+++ mypage/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
+class Member(models.Model):
+    user_id = models.CharField(max_length=50, unique=True)
+    user_pw = models.CharField(max_length=50)
+    user_name = models.CharField(max_length=50)
+    date_joined = models.DateTimeField()
 # Create your models here.
