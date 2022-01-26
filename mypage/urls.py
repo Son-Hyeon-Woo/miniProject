dce97596908e94e05ce8a8aefafbaf1c7145e3ca
Jimi <wonside99@naver.com>
2022-01-26 15:46:14 +0900

    이게 진짜

--- mypage/urls.py
+++ mypage/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('mp/', views.mp),
     path('changePW/', views.changePW),
+    path('changePW_e/', views.changePW_e),
     path('changeEM/', views.changeEM),
     path('deleteID/', views.deleteID),
 ]
