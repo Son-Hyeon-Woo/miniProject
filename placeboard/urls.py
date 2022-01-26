f32251f6cbe817483d052595df9c41f8e3532f77
minsu <excelsu1@gmail.com>
2022-01-26 11:31:49 +0900

    페이징, 글수정

--- placeboard/urls.py
+++ placeboard/urls.py
@@ -8,11 +8,12 @@ from placeboard.views import blog, posting
 app_name = 'placeboard'
 
 urlpatterns = [
-    # URL:80/blog에 접속하면 blog 페이지 + URL이름은 blog이다
+    # URL:pb/blog에 접속하면 blog 페이지 + URL이름은 blog이다
     path('blog/', blog, name='blog'),
-    # URL:80/blog/숫자로 접속하면 게시글-세부페이지(posting)
+    # URL:pb/blog/숫자로 접속하면 게시글-세부페이지(posting)
     path('blog/<int:pk>/', posting, name='posting'),
-    # path('blog/new_post/', new_post, name='new_post'),
     path('blog/new_post/', new_post, name='new_post'),
     path('blog/<int:pk>/remove/', remove_post, name='remove_post'),
+    path('blog/<int:pk>/edit/', boardEdit, name='edit'),
+
 ]
--- placeboard/views.py
+++ placeboard/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render, redirect
 # View에 Model(Post 게시글) 가져오기
-from .models import Post
+from .models import *
 from config import settings
 import os
 from .forms import FileUploadForm
 from django.contrib.auth.decorators import login_required
+from django.utils import timezone
+from django.urls import reverse
+from django.core.paginator import Paginator
 
 @login_required
 # blog.html 페이지를 부르는 blog 함수
@@ -12,9 +15,14 @@ def blog(request):
      # 모든 Post를 가져와 postlist에 저장합니다
     postlist = Post.objects.all()
     # blog.html 페이지를 열 때, 모든 Post인 postlist도 같이 가져옵니다 
-    return render(request, 'placeboard/blog.html', {'postlist':postlist})
-
+    
+    paginator = Paginator(postlist, 5)
+    page = int(request.GET.get('page', 1))
+    board_list = paginator.get_page(page)
+    return render(request, 'placeboard/blog.html',{'postlist':postlist , 'board_list':board_list})
+# , {'postlist':postlist}
 # blog의 게시글(posting)을 부르는 posting 함수
+
 def posting(request, pk):
     # 게시글(Post) 중 pk(primary_key)를 이용해 하나의 게시글(post)를 검색
     post = Post.objects.get(pk=pk)
@@ -26,6 +34,7 @@ def new_post(request):
         postname = request.POST['postname']
         contents = request.POST['contents']
         mainphoto = request.FILES["mainphoto"]
+        # mainphoto = request.POST["mainphoto"]
         post = Post(
             postname=postname,
             contents=contents,
@@ -45,5 +54,24 @@ def remove_post(request, pk):
     post = Post.objects.get(pk=pk)
     if request.method == 'POST':
         post.delete()
-        return redirect('blog')
+        return redirect('/pb/blog')
     return render(request, 'placeboard/remove_post.html', {'Post': post})
+
+
+def boardEdit(request, pk):
+    post = Post.objects.get(id=pk)
+    if request.method == "POST":
+        post.postname = request.POST['postname']
+        post.contents = request.POST['contents']
+        post.mainphoto = request.FILES['mainphoto']
+
+        post.save()
+        return redirect('/pb/blog')
+
+    else:
+        fileuploadForm = FileUploadForm
+        context = {
+            'fileuploadForm': fileuploadForm,
+        }
+        return render(request, 'placeboard/update_post.html', context)
+
